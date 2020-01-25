117b5f0e68b5ebf4f8bdaf99533b325d51dbb2fb
chinatimeline <chinatimeline@tutanota.com>
2020-01-26 02:31:12 +0800

    Date

--- assets/js/csv_to_chart.js
+++ assets/js/csv_to_chart.js
@@ -3,26 +3,34 @@ d3.csv('https://ethercalc.org/w2gfa2jtho6c.csv')
 
 var timeFormat = 'YYYY-MM-DD';
 
+function ExcelDateToJSDate(date) {
+  return new Date(Math.round((date - 25569)*86400*1000));
+}
+
+function SerialDateToJSDate(serialDate, offsetUTC) {
+  return new Date(Date.UTC(0, 0, serialDate, offsetUTC));
+}
 
 function makeChart(players) {
   var playerLabels = players.map(function(d) {return d.Date});
   var weeksData = players.map(function(d) {return d.Sum});
   var totalDeath = players.map(function(d) {return d.TotalDeath});
   var playerColors = players.map(function(d) {return d.New === '' ? '#19A0AA' : '#F15F36' ;});
+  var playerDates = players.map(function(d) {return SerialDateToJSDate(d.Date, 8).toLocaleDateString("en-US")});
 
   var chart = new Chart('chart', {
-    type: 'bar',
+    type: 'line',
     data: {
-      labels: playerLabels,
+      labels: playerDates,
       datasets: [
         {
           data: weeksData,
-          backgroundColor: '#19A0AA',
+          borderColor: '#19A0AA',
           label: '确诊人数',
         },
         {
           data: totalDeath,
-          backgroundColor: '#F15F36',
+          borderColor: '#F15F36',
           label: '死亡人数',
         },
       ]
@@ -30,9 +38,8 @@ function makeChart(players) {
     options: {
       title: {
         display: true,
-        text: '中国国内感染人数 2019-12-08 至今'
+        text: '中国大陆2019-nCoV肺炎病毒感染统计 2019-12-08 至今'
       },
-      backgroundColor: 'blue',
       tooltips: {
         mode: 'index',
         intersect: false
@@ -42,13 +49,13 @@ function makeChart(players) {
         xAxes: [{
           //type: 'time',
           time: {
-							    // parser: timeFormat,
+							    parser: timeFormat,
                   unit: 'day'
               },
           stacked: true
         }],
         yAxes: [{
-          stacked: true
+          //stacked: true
         }]
       }
     }
